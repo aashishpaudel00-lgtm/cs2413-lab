7c5dd7625ac112049561393582e7253c02cf047c
Aashish Paudel <aashishpaudel00@gmail.com>
2026-02-05 15:39:21 -0600

    your message

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,8 +1,26 @@
-#include "three_sum_sorted.h"
-
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
-    
-    // implement your code here
+    if (n < 3) {
+        return 0; //
+    }
+    for (int i = 0; i < n - 2; i++) {
+        int left = i + 1;  // This is our 'j'
+        int right = n - 1; // This is our 'k'
 
-    return 0;
+        while (left < right) {
+            int current_sum = nums[i] + nums[left] + nums[right];
+            if (current_sum == 0) {
+                *out_i = i;
+                *out_j = left;
+                *out_k = right;
+                return 1; //
+            } 
+            else if (current_sum < 0) {
+                left++; //
+            } 
+            else {
+                right--; //
+            }
+        }
+    }
+    return 0; 
 }
