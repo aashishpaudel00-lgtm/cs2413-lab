7ec322b20c3a63985d2c522c2c2bee1859a055f0
Aashish Paudel <aashishpaudel00@gmail.com>
2026-04-02 12:45:24 -0500

    Resolve merge conflicts

--- lab05/Question1/student.c
+++ lab05/Question1/student.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 Question 1: Two Sum
 
@@ -100,3 +101,29 @@ Optional helper: free all memory used by the hash table.
 static void freeTable(Node* table[]) {
     /* Write your code here if you use this helper */
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+
+// Question 1: Two Sum
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[j] == complement) {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    
+    *returnSize = 0;
+    return result;
+}
+
+>>>>>>> 349afb4 ( Finish lab05)
